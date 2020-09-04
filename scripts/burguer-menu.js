8bb9ef1a18515d179bcf0acf288465d1796524c0
Angel Leon <elonlegan@gmail.com>
2020-09-04 14:41:48 -0500

    burger-menu script in his own file

---
+++ scripts/burguer-menu.js
@@ -0,0 +1,21 @@
+const $burgerMenu = document.querySelector("#burger-menu");
+const $menu = document.querySelector(".menu");
+const ipad = window.matchMedia("(max-width: 767px)");
+runBurgerMenu(ipad.matches);
+ipad.addListener((event) => {
+  runBurgerMenu(event.matches);
+});
+function hideShow() {
+  if ($menu.classList.contains("is-active")) {
+    $menu.classList.remove("is-active");
+  } else {
+    $menu.classList.add("is-active");
+  }
+}
+function runBurgerMenu(yes) {
+  if (yes) {
+    $burgerMenu.addEventListener("click", hideShow);
+  } else {
+    $burgerMenu.removeEventListener("click", hideShow);
+  }
+}
