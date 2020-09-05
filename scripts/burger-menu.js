1fc614222053e2420341df85119fef6ce2e392f2
Angel Leon <elonlegan@gmail.com>
2020-09-04 19:55:40 -0500

    burger menu funcionality done

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -19,3 +19,18 @@ function runBurgerMenu(yes) {
     $burgerMenu.removeEventListener("click", hideShow);
   }
 }
+
+var specifiedElement = document.querySelector("nav");
+document.addEventListener("click", function (event) {
+  var isClickInsideIcon = $burgerMenu.contains(event.target);
+  var isClickInside = $menu.contains(event.target);
+  if (
+    !isClickInside &&
+    !isClickInsideIcon &&
+    $menu.classList.contains("is-active")
+  ) {
+    $menu.classList.remove("is-active");
+  } else {
+    console.log("adentro");
+  }
+});
