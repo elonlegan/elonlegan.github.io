a82ddc35014377ab43633265c9401ff2606674d5
elonlegan <elonlegan@gmail.com>
2021-10-18 15:00:18 -0500

    added skillFind function instead fetch every skill

--- scripts/api.js
+++ scripts/api.js
@@ -1,20 +1,71 @@
 var webUrl = 'https://elonlegan-api.000webhostapp.com';
-var apiUrl = '/wp-json/wp/v2/project';
+var projectUrl = '/wp-json/wp/v2/project';
+var skillUrl = '/wp-json/wp/v2/skill?per_page=99';
 
-const url = webUrl + apiUrl;
+const portfolioContainer = document.querySelector('#portfolio-container');
 
-fetch(url)
-	.then((response) => response.json())
-	.then((data) => {
-		let element = document.querySelector('#portfolio-container');
-		data.forEach(
-			(project) =>
-				(element.innerHTML += `
+var skills = [];
+
+initRequest();
+
+async function initRequest() {
+	await fetch(webUrl + skillUrl)
+		.then((response) => response.json())
+		.then((data) => {
+			skills = data;
+		})
+		.catch((err) => console.log(err));
+
+	await fetch(webUrl + projectUrl)
+		.then((response) => response.json())
+		.then((data) => insertProjects(data))
+		.catch((err) => console.log(err));
+}
+
+function dateFormatter(date) {
+	let year = date.substr(0, 4);
+	let month = date.substr(4, 2);
+	let day = date.substr(6, 2);
+
+	return day + '/' + month + '/' + year;
+}
+
+function urlPrettier(url) {
+	let urlPrettier;
+	if (url.includes('https')) {
+		urlPrettier = url.substr(8);
+	} else {
+		urlPrettier = url.substr(7);
+	}
+
+	return urlPrettier;
+}
+
+function skillInsert(skillContainerId, skills) {
+	var firstTime = true;
+	let skillContainer = document.getElementById(skillContainerId);
+	skills.forEach((skill) => {
+		skillObject = getSkill(skill.substr(60));
+		console.log(skill.substr(60));
+		console.log(skillObject);
+		if (firstTime == true) {
+			skillContainer.innerHTML = `<a href="${skillObject.url_de_certificacion}" class="tech-button tech-button--${skillObject.slug}" target="_blank" rel="noopener noreferrer">${skillObject.title.rendered}</a>
+				`;
+			firstTime = false;
+		} else {
+			skillContainer.innerHTML += `<a href="${skillObject.url_de_certificacion}" class="tech-button tech-button--${skillObject.slug}" target="_blank" rel="noopener noreferrer">${skillObject.title.rendered}</a>
+				`;
+		}
+	});
+}
+
+function insertProjects(projects) {
+	projects.forEach((project) => {
+		portfolioContainer.innerHTML += `
 			<article class="project">
             <div class="project__details">
               <h3 class="project__title">${project.title.rendered}</h3>
               <div class="project__tech" id="${project.slug}">
-								${skillInsert('#' + project.slug, project.skills)}
               </div>
               <p class="project__date">
                 <small><strong>Date: </strong>${dateFormatter(
@@ -41,49 +92,20 @@ fetch(url)
               />
             </figure>
         </article>
-			`)
-		);
+			`;
+		skillInsert(project.slug, project.skills);
+	});
 
-		document.getElementById('loader').style.display = 'none';
-		console.log(data);
-	})
-	.catch((err) => console.log(err));
-
-function dateFormatter(date) {
-	let year = date.substr(0, 4);
-	let month = date.substr(4, 2);
-	let day = date.substr(6, 2);
-
-	return day + '/' + month + '/' + year;
+	deleteLoader();
 }
 
-function urlPrettier(url) {
-	let urlPrettier;
-	if (url.includes('https')) {
-		urlPrettier = url.substr(8);
-	} else {
-		urlPrettier = url.substr(7);
-	}
-
-	return urlPrettier;
+function deleteLoader() {
+	document.getElementById('loader').style.display = 'none';
 }
 
-function skillInsert(skillContainerId, skills) {
-	var firstTime = true;
-	skills.forEach((skill) =>
-		fetch(skill)
-			.then((response) => response.json())
-			.then((skill) => {
-				let skillContainer = document.querySelector(skillContainerId);
-				if (firstTime == true) {
-					skillContainer.innerHTML = `<a href="${skill.url_de_certificacion}" class="tech-button tech-button--${skill.slug}" target="_blank" rel="noopener noreferrer">${skill.title.rendered}</a>
-					`;
-					firstTime = false;
-				} else {
-					skillContainer.innerHTML += `<a href="${skill.url_de_certificacion}" class="tech-button tech-button--${skill.slug}" target="_blank" rel="noopener noreferrer">${skill.title.rendered}</a>
-					`;
-				}
-			})
-			.catch((err) => console.log(err))
-	);
+function getSkill(skillId) {
+	console.log(skills);
+	console.log(skillId);
+	console.log(skills.find((item) => item.id == skillId));
+	return skills.find((item) => item.id == skillId);
 }
