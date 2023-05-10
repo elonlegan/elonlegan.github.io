646913cfdeb534799ebe1f3655773aa85ae56cd3
Angel Leon <angel@leanware.io>
2023-05-09 23:43:22 -0500

    ready burger menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  burgerMenuActive: boolean = false;
 
+  hideShow() {
+    console.log('hideShow');
+    this.burgerMenuActive = !this.burgerMenuActive;
+    localStorage.setItem('menu', `${this.burgerMenuActive}`);
+  }
 }
