d9cabef8bde765619360a34d75c2ef5a9208b60e
Angel Leon <angel@leanware.io>
2023-05-11 12:46:33 -0500

    hot fixes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DarkModeService } from 'angular-dark-mode';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +9,17 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   burgerMenuActive: boolean = false;
+  darkMode$: Observable<boolean> = this.darkModeService.darkMode$;
+
+  constructor(private darkModeService: DarkModeService) {}
 
   hideShow() {
     console.log('hideShow');
     this.burgerMenuActive = !this.burgerMenuActive;
     localStorage.setItem('menu', `${this.burgerMenuActive}`);
+  }
+
+  onToggle(): void {
+    this.darkModeService.toggle();
   }
 }
