ba79b5d4a17a85ea9d6737c585f3f9ac14428948
Angel Leon <angel.leon@tekus.co>
2025-09-24 02:05:45 +0200

    feat: add sitemap integration and update project dependencies
    - Added sitemap integration to astro.config.mjs for improved SEO.
    - Updated package.json and package-lock.json to include @astrojs/sitemap.
    - Refactored AboutMe.astro for clarity and conciseness.
    - Enhanced Header.astro with improved accessibility attributes.
    - Updated Hero.astro to remove CV button and adjust contact links.
    - Improved LinkButton.astro with rel attributes for security.
    - Refactored Projects.astro to use semantic HTML elements.
    - Created Skills.astro component to showcase technical skills.
    - Added new icon components for Express, Nest, Node, and others.
    - Removed components.astro page as it was no longer needed.
    - Updated index.astro to include new Skills section and improve content structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), robotsTxt()],
+	integrations: [tailwind(), sitemap(), robotsTxt()],
 	site: 'https://elonlegan.github.io',
 });
